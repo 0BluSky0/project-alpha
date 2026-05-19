61584bf597b5ded56833843084d01ae472b08b1d
Sepehr <sepehrmohammad07@gmail.com>
2026-05-19 12:16:47 +0100

    configure db file to use test database in correct evironment

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = new Pool ({
     connectionString: process.env.NODE_ENV === "test"
     ? process.env.DB_TEST_URL
-    : process.env.DB_TEST_URL
+    : process.env.DB_URL
 });
 
 module.exports = pool;
