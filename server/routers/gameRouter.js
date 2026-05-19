fdc303c1721a3e0521d34ae24e10db5758f9b160
AbuxKareem <akareembusiness@gmail.com>
2026-05-19 15:15:32 +0100

    wired middleware into routes

--- server/routers/gameRouter.js
+++ server/routers/gameRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { getQuestions, submitQuiz } = require("../controllers/gameController");
+const auth = require("../middleware/auth");
 
-router.get("/questions/:subjectId", getQuestions);
-router.post("/submit", submitQuiz);
+router.get("/questions/:subjectId", auth, getQuestions);
+router.post("/submit", auth, submitQuiz);
 module.exports = router;
--- server/routers/scoreRouter.js
+++ server/routers/scoreRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { getLeaderboard, getDashboard } = require("../controllers/scoreController");
+const auth = require("../middleware/auth");
 
 router.get("/leaderboard", getLeaderboard);
-router.get("/dashboard", getDashboard);
+router.get("/dashboard", auth, getDashboard);
 module.exports = router;
