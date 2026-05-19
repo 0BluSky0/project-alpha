fdce1b2f2c4c62d11b9dd39f33772f13dcafa9bf
Sepehr <sepehrmohammad07@gmail.com>
2026-05-19 12:17:39 +0100

    finish inital set up of testing for User class

--- server/__tests__/unit/models/User.spec.js
+++ server/__tests__/unit/models/User.spec.js
@@ -1,14 +1,36 @@
 const User = require('../../../models/User')
-const db = require('../../../database/connect')
+const db = require('../../../database/db')
 
 describe('User', () => {
 
     beforeEach(() => jest.clearAllMocks())
     afterAll(() => jest.resetAllMocks())
 
-    describe ('getAll', () => {
+    describe ('create', () => {
 
-        
+        it('should create a new user and return them', async () => {
+
+            const userData = {username: 'testUser', email: 'test@email.com', hashedPassword: 'testHashedPassword'}
+            const mockUser = { 
+                id: 1, 
+                username: 'testuser', 
+                email: 'test@email.com', 
+                password_hash: 'hashedpassword123',
+                role: 'student',
+                total_xp: 0,
+                level: 1,
+                created_at: new Date()
+            }
+
+            jest.spyOn(db, 'query').mockResolvedValueOnce({ rows: [mockUser]})
+
+            // Act
+            const result = await User.create(userData.username, userData.email, userData.hashedPassword)
+
+            //Assert
+            expect(result).toBeInstanceOf(User);
+
+        })
 
     })
 
