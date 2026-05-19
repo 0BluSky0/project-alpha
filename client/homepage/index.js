deb97dda234cc07e8b8a772eeef143f2b490846b
ella <ejgsimmons@outlook.com>
2026-05-19 10:17:56 +0100

    add signup folder aand homepage colour schemes

--- client/homepage/index.js
+++ client/homepage/index.js
@@ -17,3 +17,15 @@ document.querySelectorAll(".practice").forEach(button => {
         window.location.href="../practice/index.html"
     })
 })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  setTheme(savedTheme);
+});
+
+function setTheme(themeName) {
+  document.documentElement.setAttribute('data-theme', themeName);
+  console.log('Setting theme to:', themeName);
+  localStorage.setItem('theme', themeName);
+}
