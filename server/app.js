dce21ef39ac9d981062a1ba4a38e17e9cd48ff33
walid1099 <walidk1099@gmail.com>
2026-05-19 11:59:18 +0100

    updated syntax error in app.js and router folder

--- server/app.js
+++ server/app.js
@@ -1,26 +1,22 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require(cors());
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routers (import these once your route files exist)
-const authRouter = require("./router/Router");
+
+const authRouter = require("./routers/authRouter");
 const gameRouter = require("./routers/gameRouter");
 const scoreRouter = require("./routers/scoreRouter");
 
-app.use ("/auth, authRouter");
-app.use("/game, gameRouter")
+app.use ("/auth", authRouter); 
+app.use("/game", gameRouter);
 app.use("/scores", scoreRouter);
 
-// Mount routers
-// app.use("/auth", authRouter);
-// app.use("/questions", questionRouter);
-// app.use("/scores", scoreRouter);
 
-// Health check route - useful for testing the server is running
+
+
 app.get("/", (req, res) => {
   res.json({ message: "Server is running!" });
 });
