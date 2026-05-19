677db05e521402dd476152a3b02a9b71eeac48ce
Sepehr <sepehrmohammad07@gmail.com>
2026-05-19 14:42:28 +0100

    complete test code for create function

--- server/__tests__/unit/models/User.spec.js
+++ server/__tests__/unit/models/User.spec.js
@@ -10,12 +10,13 @@ describe('User', () => {
 
         it('should create a new user and return them', async () => {
 
-            const userData = {username: 'testUser', email: 'test@email.com', hashedPassword: 'testHashedPassword'}
+            // Arrange
+            const userData = {username: 'testuser', email: 'test@email.com', hashedPassword: 'testhashedpassword'}
             const mockUser = { 
                 id: 1, 
                 username: 'testuser', 
                 email: 'test@email.com', 
-                password_hash: 'hashedpassword123',
+                password_hash: 'testhashedpassword',
                 role: 'student',
                 total_xp: 0,
                 level: 1,
@@ -29,6 +30,25 @@ describe('User', () => {
 
             //Assert
             expect(result).toBeInstanceOf(User);
+            expect(result).toHaveProperty('username', 'testuser')
+            expect(result).toHaveProperty('email', 'test@email.com')
+            expect(result).toHaveProperty('password_hash', 'testhashedpassword')
+            expect(db.query).toHaveBeenCalledWith(
+                "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *;",
+                [userData.username, userData.email, userData.hashedPassword]
+            )
+
+        })
+
+        it('should throw an Error when no User is created', async () => {
+
+            //Arrange
+            const userData = {username: 'testuser', email: 'test@email.com', hashedPassword: 'testhashedpassword'}
+            jest.spyOn(db, 'query').mockResolvedValueOnce({rows: []})
+
+            //Act & Assert
+            await expect (User.create(userData.username, userData.email, userData.hashedPassword))
+            .rejects.toThrow('User could not be created.')
 
         })
 
