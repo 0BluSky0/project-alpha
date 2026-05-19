d263c38c79e708b05a68d84ad753e61f75fc5293
ella <ejgsimmons@outlook.com>
2026-05-19 20:01:37 +0100

    some testing and repairs - failing at token generation

--- server/routers/gameRouter.js
+++ server/routers/gameRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getQuestions, submitQuiz } = require("../controllers/gameController");
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 
-router.get("/questions/:subjectId", auth, getQuestions);
-router.post("/submit", auth, submitQuiz);
+// router.get("/questions/:subjectId", auth, getQuestions);
+// router.post("/submit", auth, submitQuiz);
+// module.exports = router;
+
+router.get("/questions/:subjectId", getQuestions);
+router.post("/submit", submitQuiz);
 module.exports = router;
