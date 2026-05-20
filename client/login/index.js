5a385833093bfead6dfab3460a52196b0ac11543
ella <ejgsimmons@outlook.com>
2026-05-20 08:52:49 +0100

    client folder - signup and login working

--- client/login/index.js
+++ client/login/index.js
@@ -2,3 +2,30 @@ function setTheme(themeName) {
   document.documentElement.setAttribute('data-theme', themeName);
   localStorage.setItem('theme', themeName);
 }
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: form.get("username"),
+            password: form.get("password")
+        })
+    }
+
+    const response = await fetch("http://localhost:3000/auth/login", options);
+    const data = await response.json();
+
+    if (response.status == 200) {
+        localStorage.setItem("token", data.token);
+        window.location.assign("../homepage/index.html");
+      } else {
+        alert(data.error);
+      }
+})
