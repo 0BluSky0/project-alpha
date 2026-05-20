edb0a3941a4cf08362ac645b9ba812fbe80e8520
Sepehr <sepehrmohammad07@gmail.com>
2026-05-20 09:35:21 +0100

    add setup file to database and change seed-db script to run that file in pacakge.json file

---
+++ server/database/setup.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+require("dotenv").config()
+
+const db = require("./db")
+
+const sql = fs.readFileSync(__dirname + '/setup.sql').toString()
+
+db.query(sql)
+  .then(data => console.log("Set-up complete."))
+  .catch(error => console.log(error))
