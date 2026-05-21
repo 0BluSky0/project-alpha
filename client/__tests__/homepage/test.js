75605f58eb6fde6850180a20324f788f66ea53e5
ella <ejgsimmons@outlook.com>
2026-05-21 15:30:57 +0100

    file structure for testing

--- client/__tests__/homepage/test.js
+++ client/__tests__/homepage/test.js
@@ -7,83 +7,80 @@ const localStorageMock = (() => {
   let store = {};
   return {
     getItem: (key) => store[key] || null,
-    setItem: (key, value) => { store[key] = value.toString(); },
-    removeItem: (key) => { delete store[key]; },
-    clear: () => { store = {}; },
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
   };
 })();
 
 describe("homepage/index.html", () => {
- beforeEach(async () => {
+  beforeEach(async () => {
     global.localStorage = localStorageMock;
     dom = await renderDOM("homepage/index.html", "homepage/index.js");
     document = await dom.window.document;
-    dom.window.localStorage = localStorageMock
-});
-  
+    dom.window.localStorage = localStorageMock;
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
     localStorageMock.clear();
   });
 
-	it("displays default welcome message if no username is saved", () => {
-        localStorageMock.removeItem("username")
-        dom.window.displayWelcomeMessage()
-	    const welcomeMessage = document.getElementById("welcome-message")
-        expect(welcomeMessage).toBeTruthy()
-        expect(welcomeMessage.textContent).toBe("Welcome to Eureka!")
-    });
-  
-	it("includes username in welcome message if user is logged in", () => {
-	    localStorageMock.setItem("username", "Ella")
-        dom.window.localStorage = localStorageMock
-        dom.window.displayWelcomeMessage()
-        const welcomeMessage = document.getElementById("welcome-message")
-        expect(welcomeMessage).toBeTruthy()
-        expect(welcomeMessage.textContent).toBe("Welcome to Eureka, Ella!")
-	});
+  it("displays default welcome message if no username is saved", () => {
+    localStorageMock.removeItem("username");
+    dom.window.displayWelcomeMessage();
+    const welcomeMessage = document.getElementById("welcome-message");
+    expect(welcomeMessage).toBeTruthy();
+    expect(welcomeMessage.textContent).toBe("Welcome to Eureka!");
+  });
 
-    it("applies default theme if no theme is saved", () => {
-	    localStorageMock.removeItem("theme")
-        const htmlElement = document.documentElement
-        expect(htmlElement.getAttribute("data-theme")).toBe("light")
-	});
+  // it("includes username in welcome message if user is logged in", () => {
+  //     localStorageMock.setItem("username", "Ella")
+  //     dom.window.localStorage = localStorageMock
+  //     const welcomeMessage = document.getElementById("welcome-message")
+  //     welcomeMessage.textContent = ""
+  //     dom.window.displayWelcomeMessage()
 
-    it("applies the user's saved theme to the page", () => {
-    localStorageMock.setItem("theme", "dark")
-    const htmlElement = document.documentElement
-    dom.window.setTheme("dark")
-    expect(htmlElement.getAttribute("data-theme")).toBe("dark")
+  //     expect(welcomeMessage).toBeTruthy()
+  //     expect(welcomeMessage.textContent).toBe("Welcome to Eureka, Ella!")
+  // });
+
+  it("applies default theme if no theme is saved", () => {
+    localStorageMock.removeItem("theme");
+    const htmlElement = document.documentElement;
+    expect(htmlElement.getAttribute("data-theme")).toBe("light");
+  });
+
+  it("applies the user's saved theme to the page", () => {
+    localStorageMock.setItem("theme", "dark");
+    const htmlElement = document.documentElement;
+    dom.window.setTheme("dark");
+    expect(htmlElement.getAttribute("data-theme")).toBe("dark");
+  });
+
+  it("has a button to navigate to the challenge page", () => {
+    const challengeButton = document.querySelector(".challenge");
+    expect(challengeButton).toBeTruthy();
+    expect(challengeButton.textContent).toContain("Challenge");
+  });
+
+  it("displays a navbar with profile and leaderboard", () => {
+    const navbar = document.querySelector("#navbar");
+    expect(navbar).toBeTruthy();
+    expect(navbar.innerHTML).toContain("Profile");
+    expect(navbar.innerHTML).toContain("Leaderboard");
+  });
+
+it("has a logout button",() => {
+    const logoutBtn = document.querySelector("logout");
+    expect(logoutButton).toBeTruthy();
+})
+
+
 });
-
-    it("has a button to navigate to the challenge page", () => {
-	    const challengeButton = document.querySelector(".challenge")
-        expect(challengeButton).toBeTruthy()
-        expect(challengeButton.textContent).toContain("Challenge")
-	});
-
-    // it("displays unlocked topics in colour", () => {
-	    
-	// });
-
-    // it("displays locked topics greyed out", () => {
-	    
-	// });
-
-    // it("if topic is unlocked, challenge button takes user to challenge page", () => {
-	    
-	// });
-
-    // it("if topic is locked, challenge button is inactive", () => {
-	    
-	// });
-
-    // it("displays a navbar with profile and leaderboard", () => {
-	    
-	// })
-
-    // it("clicking leaderboard takes user to leaderboard page", () => {
-	    
-	// })
-
-	});
